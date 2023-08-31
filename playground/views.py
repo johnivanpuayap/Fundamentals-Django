00625867d04126706d762cfb327310150941882d
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-31 21:54:52 +0800

    Learn Debugging in Django

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 # Create your views here.
 # request -> response
 # request handler
@@ -13,5 +18,6 @@ def say_hello(request):
     # Using a template
     # return render(request, 'hello.html', )
 
+    x = calculate()
     # Passing a Dictionary
     return render(request, 'hello.html', {'name': 'John Ivan Puayap'})
